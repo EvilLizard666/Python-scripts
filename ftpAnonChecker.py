a28dd9d920dc69a78ef9ae3d39b097e7a55d0067
EvilLizard666 <46286364+EvilLizard666@users.noreply.github.com>
2022-07-06 04:28:22 -1000

    Update ftpAnonChecker.py

--- ftpAnonChecker.py
+++ ftpAnonChecker.py
@@ -3,19 +3,18 @@
 from ftplib import FTP
 import sys
 
-hosts = open(sys.argv[1], 'r')
-r = hosts.readlines()
+ips = open(sys.argv[1], 'r')
+r = ips.readlines()
 for item in r:
-    print("[+] Connecting to: %s" %item)
+    item = item.strip()
+    print("[+] Connecting to: %s \n" %item)
     try:
-        
         ftp = FTP(item, timeout=3)   
         ftp.login()
-        if ftp.retrlines('LIST') != 0:
-            print("[+] Anonymous login enabled in Host:%s " %item)
-            ftp.quit()            
-        else:
-            print("[+] Anonymous is not enabled\nHost: %s" %item)
-    except:
-        print("Unable to connect to: %s" %item)
         
+        if ftp.retrlines('LIST') != 0:
+            print("[+] Anonymous login enabled on Host: %s \n" %item)
+            print("="*70+"\n")
+    except:
+        print("[+] Unable to Connect to Host: %s\n" %item)
+        print("="*70+"\n")
